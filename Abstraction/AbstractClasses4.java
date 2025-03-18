f61e27b351a03ad9a8658028cc29cb6386226402
MaddipatlaChetan24 <chetan121318@gmail.com>
2025-03-18 14:50:45 +0530

    Update AbstractClasses4.java

--- Abstraction/AbstractClasses4.java
+++ Abstraction/AbstractClasses4.java
@@ -1,1 +1,37 @@
+abstract class Sound {
+    public abstract void makeSound();
+}
+
+class Bell extends Sound {
+
+    public void makeSound() {
+        System.out.println("The bell rings: Ding dong!");
+    }
+}
+
+class Siren extends Sound {
+  
+    public void makeSound() {
+        System.out.println("The siren sounds: Woo-woo-woo!");
+    }
+}
+
+class Horn extends Sound {
+ 
+    public void makeSound() {
+        System.out.println("The horn honks: Beep beep!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Sound bell = new Bell();
+        Sound siren = new Siren();
+        Sound horn = new Horn();
+
+        bell.makeSound();
+        siren.makeSound();
+        horn.makeSound();
+    }
+}
 
