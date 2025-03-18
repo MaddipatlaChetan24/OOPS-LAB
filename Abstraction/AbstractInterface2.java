ecae22549e7c825a0ef40c0aa3bd9b82e3c73a0a
MaddipatlaChetan24 <chetan121318@gmail.com>
2025-03-18 14:53:17 +0530

    Update AbstractInterface2.java

--- Abstraction/AbstractInterface2.java
+++ Abstraction/AbstractInterface2.java
@@ -1,1 +1,28 @@
+interface Logger {
+    void logMessage(String message);
+}
+
+class FileLogger implements Logger {
+
+    public void logMessage(String message) {
+        System.out.println("Logging message to file: " + message);
+    }
+}
+
+class ConsoleLogger implements Logger {
+   
+    public void logMessage(String message) {
+        System.out.println("Logging message to console: " + message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Logger fileLogger = new FileLogger();
+        Logger consoleLogger = new ConsoleLogger();
+
+        fileLogger.logMessage("File log: User logged in.");
+        consoleLogger.logMessage("Console log: User logged in.");
+    }
+}
 
