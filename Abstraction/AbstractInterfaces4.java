3d2659f2aff1b16bdb7c91af6aa80906a5eceb9d
MaddipatlaChetan24 <chetan121318@gmail.com>
2025-03-18 14:54:29 +0530

    Update AbstractInterfaces4.java

--- Abstraction/AbstractInterfaces4.java
+++ Abstraction/AbstractInterfaces4.java
@@ -1,1 +1,28 @@
+interface Vehicle {
+    void move();
+}
+
+class Car implements Vehicle {
+
+    public void move() {
+        System.out.println("The car drives on the road.");
+    }
+}
+
+class Airplane implements Vehicle {
+
+    public void move() {
+        System.out.println("The airplane flies in the sky.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle car = new Car();
+        Vehicle airplane = new Airplane();
+
+        car.move();
+        airplane.move();
+    }
+}
 
