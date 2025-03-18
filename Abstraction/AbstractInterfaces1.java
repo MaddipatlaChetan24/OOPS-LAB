c10df2d5ac8e2d9f365cf401c67a3c3b5d79a378
MaddipatlaChetan24 <chetan121318@gmail.com>
2025-03-18 14:52:39 +0530

    Update AbstractInterfaces1.java

--- Abstraction/AbstractInterfaces1.java
+++ Abstraction/AbstractInterfaces1.java
@@ -1,1 +1,28 @@
+interface Payment {
+    void processPayment(double amount);
+}
+
+class CreditCardPayment implements Payment {
+  
+    public void processPayment(double amount) {
+        System.out.println("Processing Credit Card payment of $" + amount);
+    }
+}
+
+class PayPalPayment implements Payment {
+ 
+    public void processPayment(double amount) {
+        System.out.println("Processing PayPal payment of $" + amount);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Payment creditCard = new CreditCardPayment();
+        Payment paypal = new PayPalPayment();
+
+        creditCard.processPayment(100.50);
+        paypal.processPayment(250.75);
+    }
+}
 
