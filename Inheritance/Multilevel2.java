754af34fc7288cc1674dd554470e2dacfe80f172
MaddipatlaChetan24 <chetan121318@gmail.com>
2025-03-18 14:16:59 +0530

    Create Multilevel2.java

---
+++ Inheritance/Multilevel2.java
@@ -0,0 +1,59 @@
+class BankAccount {
+    String accountHolder;
+    double balance;
+    
+    public BankAccount(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public void displayAccountInfo() {
+        System.out.println("Account Holder: " + accountHolder + ", Balance: " + balance);
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    double interestRate;
+
+    public SavingsAccount(String accountHolder, double balance, double interestRate) {
+        super(accountHolder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public double calculateInterest() {
+        return balance * (interestRate / 100); // Interest = Principal * Rate
+    }
+
+   
+    public void displayAccountInfo() {
+        super.displayAccountInfo();
+        System.out.println("Interest Rate: " + interestRate + "%, Interest Earned: " + calculateInterest());
+    }
+}
+
+class FixedDepositAccount extends SavingsAccount {
+    int tenure; // in years
+
+    public FixedDepositAccount(String accountHolder, double balance, double interestRate, int tenure) {
+        super(accountHolder, balance, interestRate);
+        this.tenure = tenure;
+    }
+
+    public double calculateMaturityAmount() {
+        return balance + (calculateInterest() * tenure); // Maturity amount = principal + interest over years
+    }
+
+  
+    public void displayAccountInfo() {
+        super.displayAccountInfo();
+        System.out.println("Tenure: " + tenure + " years, Maturity Amount: " + calculateMaturityAmount());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        FixedDepositAccount fdAccount = new FixedDepositAccount("John Doe", 50000, 5, 10);
+        fdAccount.displayAccountInfo();
+    }
+}
+
