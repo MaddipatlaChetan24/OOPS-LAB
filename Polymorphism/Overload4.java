9c8b6a9d950f214af83b78f76f33617fd8df231f
MaddipatlaChetan24 <chetan121318@gmail.com>
2025-03-18 14:38:51 +0530

    Update Overload4.java

--- Polymorphism/Overload4.java
+++ Polymorphism/Overload4.java
@@ -1,1 +1,39 @@
+class Employee {
+
+    private String name;
+    private String department;
+    private double salary;
+
+   
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+        this.department = "Not assigned";
+    }
+
+   
+    public Employee(String name, double salary, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public void displayEmployeeDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Employee emp1 = new Employee("John", 50000);
+        emp1.displayEmployeeDetails();
+        
+      
+        Employee emp2 = new Employee("Alice", 60000, "Finance");
+        emp2.displayEmployeeDetails();
+    }
+}
 
