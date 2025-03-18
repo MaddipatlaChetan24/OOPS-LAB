02bd62ca332279734ed2992536d8a4bc9fd7a2c4
MaddipatlaChetan24 <chetan121318@gmail.com>
2025-03-18 14:41:10 +0530

    Update Override1.java

--- Polymorphism/Override1.java
+++ Polymorphism/Override1.java
@@ -1,1 +1,31 @@
+class Vehicle {
+    public void startEngine() {
+        System.out.println("The vehicle engine is starting...");
+    }
+}
+
+class Car extends Vehicle {
+
+    public void startEngine() {
+        System.out.println("The car engine is starting with a roar!");
+    }
+}
+
+class Truck extends Vehicle {
+
+    public void startEngine() {
+        System.out.println("The truck engine is starting with a heavy rumble!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle myCar = new Car();
+        Vehicle myTruck = new Truck();
+
+        
+        myCar.startEngine();  
+        myTruck.startEngine(); 
+    }
+}
 
