0942ccc8cffbf4f9e4792171ef3b3aaf8916ef1a
MaddipatlaChetan24 <chetan121318@gmail.com>
2025-03-18 14:43:37 +0530

    Update Override3.java

--- Polymorphism/Override3.java
+++ Polymorphism/Override3.java
@@ -1,1 +1,29 @@
+abstract class Payment {
+    public abstract void processPayment(double amount); 
+}
+
+class CreditCardPayment extends Payment {
+   
+    public void processPayment(double amount) {
+        System.out.println("Processing credit card payment of $" + amount);
+    }
+}
+
+class PaypalPayment extends Payment {
+
+    public void processPayment(double amount) {
+        System.out.println("Processing PayPal payment of $" + amount);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Payment creditCardPayment = new CreditCardPayment();
+        Payment paypalPayment = new PaypalPayment();
+
+        creditCardPayment.processPayment(100.50);  
+        paypalPayment.processPayment(250.75);      
+    }
+}
+
 
