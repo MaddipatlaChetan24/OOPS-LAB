3395698532dfd7a3bbd63641a898c774b4ccf552
MaddipatlaChetan24 <chetan121318@gmail.com>
2025-04-05 20:16:08 +0530

    Create Math.java

---
+++ Packages/BUILT-IN PACKAGES/Math.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+91
+CH.SC.U4CSE24030 MADDIPATLA CHETAN
+public class Main {
+public static void main(String[] args) {
+double num = 1000.0;
+double logValue = Math.log10(num);
+System.out.println("Base 10 logarithm of " + num + " is " +
+logValue);
+}
+}
